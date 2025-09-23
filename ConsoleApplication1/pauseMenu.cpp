994edbfdc0a01948628aa7988653803a0e43b64b
spyseefan@gmail.com <spyseefan@gmail.com>
2025-09-23 13:22:11 -0400

    minor fix to pause menu json and added notes

--- ConsoleApplication1/pauseMenu.cpp
+++ ConsoleApplication1/pauseMenu.cpp
@@ -43,7 +43,7 @@ void pauseMenu::loadButtons() {
 }
 
 void pauseMenu::loadJson( ) {
-	std::string fileName = "C:\\Users\\Keary\\source\\repos\\ConsoleApplication1-working\\ConsoleApplication1\\menuJsons\\pause_menu.json";
+	std::string fileName = "C:\\Users\\Keary\\source\\repos\\ConsoleApplication1-working\\ConsoleApplication1\\menuJsons\\pause_menu_background.json";
 	std::ifstream file(fileName);
 	if (!file.is_open()) {
 		logManager::logThis("Couldn't open pause menu .json");
